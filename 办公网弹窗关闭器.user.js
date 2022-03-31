5e94d3d4d8d26ff9c9b644870beec6a14bfeaa98
Roy Law <RoyLaw@users.noreply.github.com>
2022-03-31 11:38:24 +0800

    Update 办公网弹窗关闭器.user.js

--- 办公网弹窗关闭器.user.js
+++ 办公网弹窗关闭器.user.js
@@ -14,32 +14,63 @@
   'use strict';
 
   // Your code here...
+  var counter = 0;
+  var timer = null;
+  var timerList = [];
+
+  var endLoopIndicator = false;
 
   unsafeWindow.check = function () {
     return true;
   } //rip the agreement popup.
 
-  var timer = setInterval(function () {
+  function tryToFixPopups() {
     try {
 
-      if (document.querySelector('a.aui_close')) var getOut = 1; //  the last btn of this checking list.
+      if (document.querySelector('a.aui_close')) { //  the last btn of this checking list.
 
+        endLoopIndicator = true;
+        console.log("it's ture");
+
+      }
       document.querySelector('a.aui_close').click();
       //document.querySelector('div.aui_state_lock a.aui_close').click();
 
-      if (getOut) {
-        clearInterval(timer);
-        console.log("Loop ended.");
+      if (endLoopIndicator) {
+        intervalEnd();
+        console.log("Ending loop.");
       }
     }
     catch (e) {
       if (window.console) {
         window.console.log("Error in this userscript");
+        intervalEnd();
       }
     }
-  }, 1000);
+  }
 
-  this.setData({
-    interval: timer
-  });
+  function intervalStart() {
+    intervalEnd();
+    timer = window.setInterval(function () {
+      console.log("Loop started.");
+
+      tryToFixPopups();
+
+    }, 1000);
+    timerList.push(timer);
+  }
+
+  function intervalEnd() {
+    if (timer) {
+      console.log("Loop ended.");
+      timerList.forEach((item, index) => {
+        clearInterval(item);
+      })
+      timerList = [];
+      timer = 0;
+    }
+  }
+
+  intervalStart();
+
 })();
